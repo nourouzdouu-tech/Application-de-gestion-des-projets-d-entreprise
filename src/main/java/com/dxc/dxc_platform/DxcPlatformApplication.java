627171059494163b322c98de635a6470111288d0
nourouzdouu-tech <nour.ouzdouu@gmail.com>
2026-02-26 00:06:27 +0000

    first commit

---
+++ src/main/java/com/dxc/dxc_platform/DxcPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.dxc.dxc_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxcPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DxcPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dxc/dxc_platform/DxcPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dxc.dxc_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxcPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
